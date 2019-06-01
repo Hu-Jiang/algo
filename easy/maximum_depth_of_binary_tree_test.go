0284a0a989d072dcdb6e009ed183b8fc20ba547c
姜鹄 <jianghu325@gmail.com>
2019-06-01 11:42:41 +0800

    solve: maximum depth of binary tree

---
+++ easy/maximum_depth_of_binary_tree_test.go
@@ -0,0 +1,17 @@
+package easy
+
+import "testing"
+
+func TestMaxDepth(t *testing.T) {
+	root := &TreeNode{Val: 3}
+	root.Left = &TreeNode{Val: 9}
+	root.Right = &TreeNode{Val: 20}
+	root.Right.Left = &TreeNode{Val: 15}
+	root.Right.Right = &TreeNode{Val: 7}
+
+	got := maxDepth(root)
+	want := 3
+	if got != want {
+		t.Fatalf("got: %v; want: %v", got, want)
+	}
+}
