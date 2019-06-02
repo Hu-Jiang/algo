53706c961d396efce0d49a0861fd72acd6b0ce8e
姜鹄 <jianghu325@gmail.com>
2019-06-02 16:55:27 +0800

    add the official solution for problem: symmetric tree

--- easy/symmetric_tree.go
+++ easy/symmetric_tree.go
@@ -45,3 +45,50 @@ func doIsSymmetric(t1, t2 *TreeNode) bool {
 
 	return doIsSymmetric(t1.Left, t2.Right) && doIsSymmetric(t1.Right, t2.Left)
 }
+
+// Trick:
+// func doIsSymmetric(t1, t2 *TreeNode) bool {
+// 	if t1 == nil || t2 == nil {
+// 		return t1 == t2
+// 	}
+//
+// 	return t1.Val == t2.Val &&
+// 		doIsSymmetric(t1.Left, t2.Right) &&
+// 		doIsSymmetric(t1.Right, t2.Left)
+// }
+
+// The official soloution:
+//
+// Approach 1: Recursive
+//
+// public boolean isSymmetric(TreeNode root) {
+//     return isMirror(root, root);
+// }
+
+// public boolean isMirror(TreeNode t1, TreeNode t2) {
+//     if (t1 == null && t2 == null) return true;
+//     if (t1 == null || t2 == null) return false;
+//     return (t1.val == t2.val)
+//         && isMirror(t1.right, t2.left)
+//         && isMirror(t1.left, t2.right);
+// }
+//
+// Approach 2: Iterative
+//
+// public boolean isSymmetric(TreeNode root) {
+//     Queue<TreeNode> q = new LinkedList<>();
+//     q.add(root);
+//     q.add(root);
+//     while (!q.isEmpty()) {
+//         TreeNode t1 = q.poll();
+//         TreeNode t2 = q.poll();
+//         if (t1 == null && t2 == null) continue;
+//         if (t1 == null || t2 == null) return false;
+//         if (t1.val != t2.val) return false;
+//         q.add(t1.left);
+//         q.add(t2.right);
+//         q.add(t1.right);
+//         q.add(t2.left);
+//     }
+//     return true;
+// }
