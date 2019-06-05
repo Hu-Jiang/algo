de3cc3ecf0c5c957794cc3054cc8baa7f03bb865
姜鹄 <jianghu325@gmail.com>
2019-06-05 14:00:57 +0800

    add official soloution for problem: find duplicate file in system

--- medium/find_duplicate_file_in_system.go
+++ medium/find_duplicate_file_in_system.go
@@ -73,3 +73,65 @@ func splitNameContent(file string) (name, content string) {
 	content = strings.Split(strings.Split(file, "(")[1], ")")[0]
 	return
 }
+
+// The official soloution:
+//
+// Approach #1 Brute Force [Time Limit Exceeded]
+//
+// public class Solution {
+//     public List < List < String >> findDuplicate(String[] paths) {
+//         List < String[] > list = new ArrayList < > ();
+//         for (String path: paths) {
+//             String[] values = path.split(" ");
+//             for (int i = 1; i < values.length; i++) {
+//                 String[] name_cont = values[i].split("\\(");
+//                 name_cont[1] = name_cont[1].replace(")", "");
+//                 list.add(new String[] {
+//                     values[0] + "/" + name_cont[0], name_cont[1]
+//                 });
+//             }
+//         }
+//         boolean[] visited = new boolean[list.size()];
+//         List < List < String >> res = new ArrayList < > ();
+//         for (int i = 0; i < list.size() - 1; i++) {
+//             if (visited[i])
+//                 continue;
+//             List < String > l = new ArrayList < > ();
+//             for (int j = i + 1; j < list.size(); j++) {
+//                 if (list.get(i)[1].equals(list.get(j)[1])) {
+//                     l.add(list.get(j)[0]);
+//                     visited[j] = true;
+//                 }
+//             }
+//             if (l.size() > 0) {
+//                 l.add(list.get(i)[0]);
+//                 res.add(l);
+//             }
+//         }
+//         return res;
+//     }
+// }
+//
+// Approach #2 Using HashMap [Accepted]
+//
+// public class Solution {
+//     public List < List < String >> findDuplicate(String[] paths) {
+//         HashMap < String, List < String >> map = new HashMap < > ();
+//         for (String path: paths) {
+//             String[] values = path.split(" ");
+//             for (int i = 1; i < values.length; i++) {
+//                 String[] name_cont = values[i].split("\\(");
+//                 name_cont[1] = name_cont[1].replace(")", "");
+//                 List < String > list = map.getOrDefault(name_cont[1], new ArrayList < String > ());
+//                 list.add(values[0] + "/" + name_cont[0]);
+//                 map.put(name_cont[1], list);
+//             }
+//         }
+//         List < List < String >> res = new ArrayList < > ();
+//         for (String key: map.keySet()) {
+//             if (map.get(key).size() > 1)
+//                 res.add(map.get(key));
+//         }
+//         return res;
+//     }
+// }
