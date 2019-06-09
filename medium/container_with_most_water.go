b19ed4f202a1a0815f0f5b74e44c9800c9c734d6
姜鹄 <jianghu325@gmail.com>
2019-06-09 20:14:36 +0800

    add official soloution for problem: container with most water

--- medium/container_with_most_water.go
+++ medium/container_with_most_water.go
@@ -39,3 +39,33 @@ func area(hl, hr, width int) int {
 		return hl * width
 	}
 }
+
+// The official soloution:
+//
+// Approach 1: Brute Force
+//
+// public class Solution {
+//     public int maxArea(int[] height) {
+//         int maxarea = 0;
+//         for (int i = 0; i < height.length; i++)
+//             for (int j = i + 1; j < height.length; j++)
+//                 maxarea = Math.max(maxarea, Math.min(height[i], height[j]) * (j - i));
+//         return maxarea;
+//     }
+// }
+//
+// Approach 2: Two Pointer Approach
+//
+// public class Solution {
+//     public int maxArea(int[] height) {
+//         int maxarea = 0, l = 0, r = height.length - 1;
+//         while (l < r) {
+//             maxarea = Math.max(maxarea, Math.min(height[l], height[r]) * (r - l));
+//             if (height[l] < height[r])
+//                 l++;
+//             else
+//                 r--;
+//         }
+//         return maxarea;
+//     }
+// }
