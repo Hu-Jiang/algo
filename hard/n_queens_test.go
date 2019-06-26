b7a2c1c5238d9dbf47c8bfaa0238fa591be84b51
姜鹄 <jianghu325@gmail.com>
2019-06-26 19:22:31 +0800

    update file name, use '_' to substitute '-'

---
+++ hard/n_queens_test.go
@@ -0,0 +1,39 @@
+package hard
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSolveNQueues(t *testing.T) {
+	tests := []struct {
+		n    int
+		want [][]string
+	}{
+		{0, [][]string{}},
+		{1, [][]string{[]string{"Q"}}},
+		{2, [][]string{}},
+		{3, [][]string{}},
+		{4, [][]string{
+			[]string{
+				".Q..",
+				"...Q",
+				"Q...",
+				"..Q.",
+			},
+			[]string{
+				"..Q.",
+				"Q...",
+				"...Q",
+				".Q..",
+			},
+		}},
+	}
+
+	for i, tt := range tests {
+		got := solveNQueens(tt.n)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Fatalf("%d: got: %v; want: %v", i, got, tt.want)
+		}
+	}
+}
