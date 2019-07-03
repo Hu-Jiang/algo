51a65565e2f7a6e76b0b7fe03185e9fe13626019
姜鹄 <jianghu325@gmail.com>
2019-07-03 21:03:00 +0800

    solve: unique paths ii

---
+++ medium/unique_paths_ii_test.go
@@ -0,0 +1,55 @@
+package medium
+
+import (
+	"testing"
+)
+
+func TestUniquePathsWithObstacles(t *testing.T) {
+	tests := []struct {
+		obstacleGrid [][]int
+		want         int
+	}{
+		{nil, 0},
+		{
+			[][]int{
+				[]int{1, 0},
+			},
+			0,
+		},
+		{
+			[][]int{
+				[]int{0, 1, 0},
+			},
+			0,
+		},
+		{
+			[][]int{
+				[]int{0, 0, 0},
+				[]int{0, 0, 0},
+			},
+			3,
+		},
+		{
+			[][]int{
+				[]int{0, 0, 0},
+				[]int{0, 1, 0},
+				[]int{0, 0, 0}},
+			2,
+		},
+		{
+			[][]int{
+				[]int{0, 0, 0},
+				[]int{0, 0, 1},
+				[]int{0, 1, 0},
+			},
+			0,
+		},
+	}
+
+	for i, tt := range tests {
+		got := uniquePathsWithObstacles(tt.obstacleGrid)
+		if got != tt.want {
+			t.Fatalf("#%d. got %v, want %v", i, got, tt.want)
+		}
+	}
+}
