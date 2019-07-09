84e84bd8c26ea045b03b3ecd760307c73923cf6a
姜鹄 <jianghu325@gmail.com>
2019-07-09 20:37:19 +0800

    solve: word search

---
+++ medium/word_search.go
@@ -0,0 +1,55 @@
+package medium
+
+// Given a 2D board and a word, find if the word exists in the grid.
+//
+// The word can be constructed from letters of sequentially adjacent
+// cell, where "adjacent" cells are those horizontally or vertically
+// neighboring. The same letter cell may not be used more than once.
+//
+// Example:
+// board =
+// [
+//   ['A','B','C','E'],
+//   ['S','F','C','S'],
+//   ['A','D','E','E']
+// ]
+// Given word = "ABCCED", return true.
+// Given word = "SEE", return true.
+// Given word = "ABCB", return false.
+
+func exist(board [][]byte, word string) bool {
+	if len(board) == 0 || len(word) == 0 {
+		return false
+	}
+	for i := 0; i < len(board); i++ {
+		for j := 0; j < len(board[0]); j++ {
+			if dfsMatch(board, word, i, j) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func dfsMatch(board [][]byte, word string, i, j int) bool {
+	if len(word) == 0 {
+		return true
+	}
+	if board[i][j] != word[0] {
+		return false
+	}
+	if len(word) == 1 {
+		return true
+	}
+	tmp := board[i][j]
+	board[i][j] = 0
+	isMatched := (j+1 < len(board[0]) && dfsMatch(board, word[1:], i, j+1)) ||
+		(j-1 >= 0 && dfsMatch(board, word[1:], i, j-1)) ||
+		(i+1 < len(board) && dfsMatch(board, word[1:], i+1, j)) ||
+		(i-1 >= 0 && dfsMatch(board, word[1:], i-1, j))
+	board[i][j] = tmp
+	if isMatched {
+		return true
+	}
+	return false
+}
---
+++ medium/word_search_test.go
@@ -0,0 +1,51 @@
+package medium
+
+import (
+	"testing"
+)
+
+func TestExist(t *testing.T) {
+	tests := []struct {
+		board [][]byte
+		word  string
+		want  bool
+	}{
+		{nil, "", false},
+		{[][]byte{[]byte{'a'}}, "a", true},
+		{[][]byte{[]byte{'a', 'a'}}, "aaa", false},
+		{
+			[][]byte{
+				{'A', 'B', 'C', 'E'},
+				{'S', 'F', 'C', 'S'},
+				{'A', 'D', 'E', 'E'},
+			},
+			"ABCCED",
+			true,
+		},
+		{
+			[][]byte{
+				{'A', 'B', 'C', 'E'},
+				{'S', 'F', 'C', 'S'},
+				{'A', 'D', 'E', 'E'},
+			},
+			"SEE",
+			true,
+		},
+		{
+			[][]byte{
+				{'A', 'B', 'C', 'E'},
+				{'S', 'F', 'C', 'S'},
+				{'A', 'D', 'E', 'E'},
+			},
+			"ABCD",
+			false,
+		},
+	}
+
+	for i, tt := range tests {
+		got := exist(tt.board, tt.word)
+		if got != tt.want {
+			t.Fatalf("#%d. got %v, want %v", i, got, tt.want)
+		}
+	}
+}
