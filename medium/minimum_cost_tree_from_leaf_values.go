4389f822ada3b9fb24fb7ade2154746d1c4f6647
姜鹄 <jianghu325@gmail.com>
2019-07-24 22:02:05 +0800

    add dp solution

--- medium/minimum_cost_tree_from_leaf_values.go
+++ medium/minimum_cost_tree_from_leaf_values.go
@@ -73,3 +73,32 @@ func minLeaf(a, b int) int {
 	}
 	return b
 }
+
+// DP solution from leetcode discuss:
+// 递推公式dp[i][j]代表节点从i到j所能构成的非叶节点的最小和:
+// 			dp[i][j] = min(dp[i][j],dp[i][k] + dp[k+1][j] + max[i][k]*max[k+1][j])
+// 那么,最终我们要求的结果就是 dp[0][n-1]的值.枚举过程是先枚举区间长度l,再枚举起始点s,
+// 根据起始点和长度算出区间终点s+l,最后枚举区间中的点k.
+//
+// class Solution:
+//     def mctFromLeafValues(self, arr: List[int]) -> int:
+//         n = len(arr)
+//         # 初始值设为最大
+//         dp = [[float('inf') for _ in range(n)] for _ in range(n)]
+//         # 初始区间查询最大值设为0
+//         maxval = [[0 for _ in range(n)] for _ in range(n)]
+//         # 求区间[i, j]中最大元素
+//         for i in range(n):
+//             for j in range(i, n):
+//                 maxval[i][j] = max(arr[i:j + 1])
+//         # 叶子结点不参与计算
+//         for i in range(n):
+//             dp[i][i] = 0
+//         # 枚举区间长度
+//         for l in range(1, n):
+//             # 枚举区间起始点
+//             for s in range(n - l):
+//                 # 枚举划分两棵子树
+//                 for k in range(s, s + l):
+//                     dp[s][s + l] = min(dp[s][s + l], dp[s][k] + dp[k + 1][s + l] + maxval[s][k] * maxval[k + 1][s + l])
+//         return dp[0][n - 1]
